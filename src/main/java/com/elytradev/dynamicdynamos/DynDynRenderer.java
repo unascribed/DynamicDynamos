90afebfb91903f741ab0dc2998932fa2fcabaeed
Paul <Paul.Hancock.17041993@live.com>
2018-12-14 05:11:44 +1100

    Updates and box fix (#3)
    Updated to work with modern changes, and the classic box replaced with a custom one for texture consistency

--- src/main/java/com/elytradev/dynamicdynamos/DynDynRenderer.java
+++ src/main/java/com/elytradev/dynamicdynamos/DynDynRenderer.java
@@ -13,12 +13,13 @@ import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.MathHelper;
+import net.minecraftforge.fml.common.FMLLog;
 
 public class DynDynRenderer extends TileEntitySpecialRenderer<TileDynamoBase> {
 	private ModelPump model = new ModelPump();
-	
+
 	private Map<String, ResourceLocation> resLocCache = Maps.newHashMap();
-	
+
 	@Override
 	public void render(TileDynamoBase dyn, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
 		if (dyn == null) return;
@@ -30,15 +31,15 @@ public class DynDynRenderer extends TileEntitySpecialRenderer<TileDynamoBase> {
 			} else {
 				speed = 0;
 			}
-			
+
 			float pumpX = 0f;
 			float pumpY = 0f;
 			float pumpZ = -0.25f;
-			
+
 			float stroke = (MathHelper.sin((ClientProxy.stroke.get(dyn).floatValue()+(partialTicks*speed))/10f)+1f)/2f;
-			
+
 			pumpZ -= (stroke)*(0.245f);
-			
+
 			GlStateManager.pushMatrix();
 				GlStateManager.translate(x+0.5, y+0.5, z+0.5);
 				switch (EnumFacing.getFront(dyn.getFacing())) {
@@ -62,7 +63,9 @@ public class DynDynRenderer extends TileEntitySpecialRenderer<TileDynamoBase> {
 						break;
 				}
 				GlStateManager.translate(pumpX-(5/16f), pumpY-(5/16f), pumpZ);
-				TextureAtlasSprite tas = TETextures.DYNAMO[dyn.getType()];
+
+				TextureAtlasSprite tas = TETextures.DYNAMO[dyn.getBlockMetadata()];
+
 				String[] split = tas.getIconName().split(":");
 				Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation(split[0], "textures/"+split[1]+".png"));
 				model.render(null, 0, 0, 0, 0, 0, partialTicks);
